2529f972b14df562a8f30cab96cb7b7c1727e60f
iaMODE <noticiasbedeje@gmail.com>
2026-05-24 09:00:16 -0400

    Backup antes de implementar firma por foto limpia

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = create_app()
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8080)),
-        debug=False
+        port=5000,
+        debug=True
     )
